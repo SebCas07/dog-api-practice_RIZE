81d2704a4c3adeafc161b0445c80f83406f93b91
SebCas07 <tiancas07@gmail.com>
2026-03-29 21:49:00 -0500

    Practice saved for references

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 console.log("script.js loaded");
 
+let data = fetch("https://dogapi.dog/api/facts"); 
+console.log(data); 
+
+async function getDogFact() { 
+    const response = await fetch("https://dogapi.dog/api/facts"); 
+    const data = await response.json(); 
+    const fact = data.facts[0]; 
+
+    //update the DOM with the dog fact 
+    const output = document.getElementById("dog-fact-output"); 
+    output.textContent = fact; 
+}
+
+// getDogFact()
+
+const button = document.getElementById("fetch-dog-btn"); 
+
+button.addEventListener("click", function() { 
+    getDogFact()
+});
